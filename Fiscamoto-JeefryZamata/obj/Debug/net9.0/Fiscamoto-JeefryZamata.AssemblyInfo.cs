f62b31112f0755471eb7d58095f8b8ee692b67b5
jeefry.zamata@tecsup.edu.pe <jeefry.zamata@tecsup.edu.pe>
2025-11-20 14:59:12 -0500

    patch: Add RuntimeIdentifiers in csproj

--- Fiscamoto-JeefryZamata/obj/Debug/net9.0/Fiscamoto-JeefryZamata.AssemblyInfo.cs
+++ Fiscamoto-JeefryZamata/obj/Debug/net9.0/Fiscamoto-JeefryZamata.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fiscamoto-JeefryZamata")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5adf584b9bc53c023567368f80cd42808257766")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fiscamoto-JeefryZamata")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fiscamoto-JeefryZamata")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
